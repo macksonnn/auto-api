4af4e0cbd098376d0cb93db4a73e9088dc2102e5
Lucas Massena <lucas@massena.com.br>
2024-09-22 20:42:32 -0300

    New TicketCreatedEventHandler

---
+++ src/Core/Core.Application/Ticket/Events/TicketCreatedEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using AutoMais.Ticket.Core.Application.Ticket.Stream;
+using AutoMais.Ticket.Core.Domain.Aggregates.Ticket.Events;
+using Becape.Core.Common.Stream;
+
+namespace AutoMais.Ticket.Core.Application.Ticket.Events
+{
+    internal class TicketCreatedEventHandler (IStream stream) : INotificationHandler<TicketCreated>
+    {
+        public Task Handle(TicketCreated notification, CancellationToken cancellationToken)
+        {
+            return stream.SendEventAsync(notification, TicketStream.TicketCreated);
+        }
+    }
+}
