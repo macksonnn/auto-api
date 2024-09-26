fc6c712062c9de0183d167a50cd73a3a165b8487
Allexz <jallexz@msn.com>
2024-09-26 19:41:39 -0300

    Seguranca

--- src/Core/Core.Application/Attendant/Queries/AttendantGet.cs
+++ src/Core/Core.Application/Attendant/Queries/AttendantGet.cs
@@ -1,14 +1,9 @@
 ﻿using AutoMais.Ticket.Core.Domain.Aggregates.Attendant;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutoMais.Ticket.Core.Application.Attendant.Queries;
-public record AttendantGet : QueryOneBase<AttendantAgg>
+public record AttendantGetOne : QueryOneBase<AttendantAgg>
 {
-    public AttendantGet(string id)
+    public AttendantGetOne(string id)
     {
         Id = id.ToLower();
     }
---
+++ src/Core/Core.Application/Attendant/Queries/AttendantGetQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using AutoMais.Ticket.Core.Application.Attendant.State;
+using AutoMais.Ticket.Core.Domain.Aggregates.Attendant;
+
+namespace AutoMais.Ticket.Core.Application.Attendant.Queries;
+public class AttendantGetQueryHandler(IAttendantState state) : IRequestHandler<AttendantGetOne, Result<AttendantAgg>>
+{
+    public async Task<Result<AttendantAgg>> Handle(AttendantGetOne request, CancellationToken cancellationToken)
+    {
+        return await state.Get(request.Id);
+    }
+}
+
+     
