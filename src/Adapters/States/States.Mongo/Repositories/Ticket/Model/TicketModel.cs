5f1125f489362a9dfec434224e2951960d56de97
Lucas Massena <lucas@massena.com.br>
2024-09-30 23:10:05 -0300

    Removing unused code

--- src/Adapters/States/States.Mongo/Repositories/Ticket/Model/TicketModel.cs
+++ src/Adapters/States/States.Mongo/Repositories/Ticket/Model/TicketModel.cs
@@ -1,42 +1,42 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
-using MongoDB.Bson;
-using FluentResults;
-using AutoMais.Ticket.Core.Domain.Aggregates.Ticket;
+﻿//using MongoDB.Bson.Serialization.Attributes;
+//using MongoDB.Bson;
+//using FluentResults;
+//using AutoMais.Ticket.Core.Domain.Aggregates.Ticket;
 
-namespace States.Mongo.Repositories.Ticket.Model;
+//namespace States.Mongo.Repositories.Ticket.Model;
 
-public class TicketModel
-{
-    [BsonId]
-    [BsonElement("_id")]
-    public ObjectId _Id { get; set; }
-    [BsonRepresentation(BsonType.String)]
-    public string ID { get; set; }
-    public string AttendantId { get; private set; }
-    public DateTime CreatedDate { get; private set; }
-    public DateTime? AbandonedDate { get; private set; }
-    public DateTime? ClosedDate { get; private set; }
-    public string Description { get; private set; }
+//public class TicketModel
+//{
+//    [BsonId]
+//    [BsonElement("_id")]
+//    public ObjectId _Id { get; set; }
+//    [BsonRepresentation(BsonType.String)]
+//    public string ID { get; set; }
+//    public string AttendantId { get; private set; }
+//    public DateTime CreatedDate { get; private set; }
+//    public DateTime? AbandonedDate { get; private set; }
+//    public DateTime? ClosedDate { get; private set; }
+//    public string Description { get; private set; }
 
-    internal static TicketModel FromDomain(TicketAgg ticket)
-    {
-        return new TicketModel()
-        {
-            ID = ticket.Id,
-            Description = ticket.Description,
-            CreatedDate = ticket.CreatedDate,
-            AbandonedDate = ticket.AbandonedDate,
-            ClosedDate = ticket.ClosedDate
-        };
-    }
+//    internal static TicketModel FromDomain(TicketAgg ticket)
+//    {
+//        return new TicketModel()
+//        {
+//            ID = ticket.Id,
+//            Description = ticket.Description,
+//            CreatedDate = ticket.CreatedDate,
+//            AbandonedDate = ticket.AbandonedDate,
+//            ClosedDate = ticket.ClosedDate
+//        };
+//    }
 
-    internal static Result<TicketAgg> ToDomain(TicketModel model)
-    {
-        if (model is null)
-            return Result.Fail<TicketAgg>("Ticket not found");
+//    internal static Result<TicketAgg> ToDomain(TicketModel model)
+//    {
+//        if (model is null)
+//            return Result.Fail<TicketAgg>("Ticket not found");
 
-        var ticketResult = TicketAgg.Create(model.ID, model.AttendantId, model.Description, model.CreatedDate, model.AbandonedDate, model.ClosedDate);
+//        var ticketResult = TicketAgg.Create(model.ID, model.AttendantId, model.Description, model.CreatedDate, model.AbandonedDate, model.ClosedDate);
 
-        return ticketResult;
-    }
-}
+//        return ticketResult;
+//    }
+//}
