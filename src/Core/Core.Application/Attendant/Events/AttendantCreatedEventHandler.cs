6c1f2b7388ccec5dc1280691c940e243a9be77fc
Lucas Massena <lucas@massena.com.br>
2024-09-30 22:44:58 -0300

    Disabling event publish

--- src/Core/Core.Application/Attendant/Events/AttendantCreatedEventHandler.cs
+++ src/Core/Core.Application/Attendant/Events/AttendantCreatedEventHandler.cs
@@ -1,10 +1,10 @@
-﻿using AutoMais.Ticket.Core.Application.Attendant.Stream;
-using AutoMais.Ticket.Core.Domain.Aggregates.Attendant.Events;
-using Becape.Core.Common.Stream;
+﻿//using AutoMais.Ticket.Core.Application.Attendant.Stream;
+//using AutoMais.Ticket.Core.Domain.Aggregates.Attendant.Events;
+//using Becape.Core.Common.Stream;
 
-namespace AutoMais.Ticket.Core.Application.Attendant.Events;
-internal class AttendantCreatedEventHandler(IStream stream) : INotificationHandler<AttendantCreated>
-{
-    public Task Handle(AttendantCreated notification, CancellationToken cancellationToken)
-     =>   stream.SendEventAsync(notification, AttendantStream.AttendantCreated) ;
-}
+//namespace AutoMais.Ticket.Core.Application.Attendant.Events;
+//internal class AttendantCreatedEventHandler(IStream stream) : INotificationHandler<AttendantCreated>
+//{
+//    public Task Handle(AttendantCreated notification, CancellationToken cancellationToken)
+//     =>   stream.SendEventAsync(notification, AttendantStream.AttendantCreated) ;
+//}
