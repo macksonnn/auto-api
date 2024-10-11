a75a011ac347ca9567604699752a0002dea7aaef
Lucas Massena <lucas@massena.com.br>
2024-10-11 12:43:29 -0300

    Adding

--- src/Adapters/Web/API/Controllers/VehicleController.cs
+++ src/Adapters/Web/API/Controllers/VehicleController.cs
@@ -1,9 +1,14 @@
 ﻿using AutoMais.Ticket.Core.Application.Vehicle.Adapters.Services;
+using AutoMais.Ticket.Core.Domain.Aggregates.Vehicle;
+using FluentResults;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Net;
 
 namespace AutoMais.Ticket.Api.Controllers
 {
     public class VehicleController : IEndpointDefinition
-    {
+    {      
         public void RegisterEndpoints(RouteGroupBuilder app)
         {
             var v1 = app.MapGroup("/v1/vehicles").WithTags("Vehicles");
@@ -13,7 +18,11 @@ namespace AutoMais.Ticket.Api.Controllers
                 var result = await plateService.GetPlate(plate);
 
                 return result;
-            });
+            }).WithOpenApi(o => new(o)
+            {
+                Summary = "Retrieve vehicle details based on the plate.",
+                Description = "If the document does not exists in our records, then retrieve from an external service and this operation has extra costs."
+            }).Produces<VehicleAgg>();
         }
     }
 }
