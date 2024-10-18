b9c6506ea7ac9cf192d5bb5dd53f6abb1b938c5f
Lucas Massena <lucas@massena.com.br>
2024-10-18 02:57:11 -0300

    Change vehicle restriction after 5 minutes

--- src/Core/Core.Domain/Aggregates/Ticket/Vehicle.cs
+++ src/Core/Core.Domain/Aggregates/Ticket/Vehicle.cs
@@ -9,9 +9,11 @@ namespace AutoMais.Ticket.Core.Domain.Aggregates.Ticket
         public string Brand { get; private set; }
         public string Model { get; private set; }
         public string Color { get; private set; }
+        public string Fuel { get; private set; }
         public string Situation { get; private set; }
         public string Image { get; private set; }
         public VehicleTypeEnum Type { get; private set; }
+        public DateTime LastChangeDate { get; private set; }
 
 
         public static Vehicle Create(VehicleAgg vehicle)
@@ -22,8 +24,10 @@ namespace AutoMais.Ticket.Core.Domain.Aggregates.Ticket
                 Brand = vehicle.Brand,
                 Model = vehicle.Model,
                 Color = vehicle.Color,
+                Fuel = vehicle.Fuel,
                 Situation = vehicle.Situation,
                 Image = vehicle.Image,
+                LastChangeDate = DateTime.Now,
                 Type = VehicleTypeEnum.Truck //TODO: Revisar como mapenar os tipos para o enum
             };
         }
