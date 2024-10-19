177613124fcdf6d9e7fed5b140747877d2163975
Lucas Massena <lucas@massena.com.br>
2024-10-19 19:06:11 -0300

    Grouping classes

--- src/Core/Core.Application/Ticket/Commands/UpdateFuelToTicketCommandHandler.cs
+++ src/Core/Core.Application/Ticket/Commands/UpdateFuelToTicketCommandHandler.cs
@@ -7,6 +7,42 @@ using AutoMais.Ticket.Core.Domain.Aggregates.Ticket.Events;
 
 namespace AutoMais.Ticket.Core.Application.Ticket.Commands
 {
+    /// <summary>
+    /// The command validator contains the rules to ensure the object is valid
+    /// </summary>
+    public sealed class AddFuelToTicketCommandValidator : AbstractValidator<AddFuelToTicketCommand>
+    {
+        public AddFuelToTicketCommandValidator(IPumpState pumpState, IAttendantState attendantState)
+        {
+            RuleFor(command => command.CardId)
+                .NotEmpty()
+                .MustAsync(async (instance, propValue, c) =>
+                {
+                    instance.Attendant = await attendantState.Get(x => x.CardId == propValue);
+                    return instance.Attendant != null;
+                });
+
+            RuleFor(command => command.PumpNumber)
+                .NotEmpty()
+                .MustAsync(async (instance, propValue, cancellationToken) =>
+                {
+                    instance.Pump = await pumpState.Get(x => x.Number == propValue);
+                    return instance.Pump != null;
+                }).DependentRules(() =>
+                {
+                    RuleFor(command => command.NozzleNumber)
+                        .NotEmpty()
+                        .Must((instance, property, token) =>
+                        {
+                            instance.Nozzle = instance?.Pump?.Nozzles?.FirstOrDefault(x => x.Number == property);
+                            return instance?.Nozzle != null;
+                        });
+                });
+
+            RuleFor(command => command.Quantity)
+                .NotEmpty();
+        }
+    }
 
     /// <summary>
     /// The command validator contains the rules to ensure the object is valid
@@ -68,8 +104,47 @@ namespace AutoMais.Ticket.Core.Application.Ticket.Commands
 
     public class UpdateFuelToTicketCommandHandler(ITicketState ticketState, IMediator mediator) : 
         ICommandHandler<FinishSupply, TicketUpdated>,
-        ICommandHandler<UpdateFuelToTicketCommand, TicketUpdated>
+        ICommandHandler<UpdateFuelToTicketCommand, TicketUpdated>,
+        ICommandHandler<AddFuelToTicketCommand, TicketUpdated>
     {
+        public async Task<Result<TicketUpdated>> Handle(AddFuelToTicketCommand command, CancellationToken cancellationToken)
+        {
+            var openedTicket = await ticketState.GetOpenedTicket(command);
+
+            if (openedTicket == null)
+            {
+                var ticketCreated = await mediator.Send(new CreateTicketForAttendantCommand(command.CardId, command.PumpNumber, command.NozzleNumber));
+
+                if (ticketCreated.IsSuccess)
+                    openedTicket = Result.Ok(ticketCreated.Value.Ticket);
+                else
+                    return Result.Fail<TicketUpdated>("Failure creating ticket").WithErrors(ticketCreated.Errors);
+            }
+
+            var result = Result.Ok();
+
+            if (openedTicket != null)
+            {
+                var updated = openedTicket.Value.AddOrUpdateSupply(command);
+
+                if (updated.IsSuccess)
+                {
+                    var saved = await ticketState.Update(openedTicket.Value.Id, updated.Value.Ticket);
+                    if (saved.IsSuccess)
+                    {
+                        await mediator.Publish(updated.Value);
+                        return updated.Value;
+                    }
+                    else
+                        result.WithErrors(saved.Errors);
+                }
+
+                result.WithErrors(updated.Errors);
+            }
+
+            return result;
+        }
+
         public async Task<Result<TicketUpdated>> Handle(UpdateFuelToTicketCommand command, CancellationToken cancellationToken)
         {
             var openedTicket = await ticketState.GetOpenedTicket(command);
