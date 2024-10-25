856f2e3eca6d305bcff2452961aa012998584c7c
Lucas Massena <lucas@massena.com.br>
2024-10-25 16:48:39 -0300

    Removing event handlers

--- src/Core/Core.Application/Ticket/Events/TicketCreatedEventHandler.cs
+++ src/Core/Core.Application/Ticket/Events/TicketCreatedEventHandler.cs
@@ -1,14 +1,14 @@
-﻿using AutoMais.Core.Common.Stream;
-using AutoMais.Ticket.Core.Application.Ticket.Adapters.Stream;
-using AutoMais.Ticket.Core.Domain.Aggregates.Ticket.Events;
+﻿//using AutoMais.Core.Common.Stream;
+//using AutoMais.Ticket.Core.Application.Ticket.Adapters.Stream;
+//using AutoMais.Ticket.Core.Domain.Aggregates.Ticket.Events;
 
-namespace AutoMais.Ticket.Core.Application.Ticket.Events
-{
-    internal class TicketCreatedEventHandler(IStream stream) : INotificationHandler<TicketCreated>
-    {
-        public Task Handle(TicketCreated notification, CancellationToken cancellationToken)
-        {
-            return stream.SendEventAsync(notification, TicketStream.TicketCreated);
-        }
-    }
-}
+//namespace AutoMais.Ticket.Core.Application.Ticket.Events
+//{
+//    internal class TicketCreatedEventHandler(IStream stream) : INotificationHandler<TicketCreated>
+//    {
+//        public Task Handle(TicketCreated notification, CancellationToken cancellationToken)
+//        {
+//            return stream.SendEventAsync(notification, TicketStream.TicketCreated);
+//        }
+//    }
+//}
--- src/Core/Core.Application/Ticket/Events/TicketProductsChangedEventHandler.cs
+++ src/Core/Core.Application/Ticket/Events/TicketProductsChangedEventHandler.cs
@@ -1,14 +1,14 @@
-﻿using AutoMais.Core.Common.Stream;
-using AutoMais.Ticket.Core.Application.Ticket.Adapters.Stream;
-using AutoMais.Ticket.Core.Domain.Aggregates.Ticket.Events;
+﻿//using AutoMais.Core.Common.Stream;
+//using AutoMais.Ticket.Core.Application.Ticket.Adapters.Stream;
+//using AutoMais.Ticket.Core.Domain.Aggregates.Ticket.Events;
 
-namespace AutoMais.Ticket.Core.Application.Ticket.Events
-{
-    internal class TicketProductsChangedEventHandler(IStream stream) : INotificationHandler<TicketProductsChanged>
-    {
-        public Task Handle(TicketProductsChanged notification, CancellationToken cancellationToken)
-        {
-            return stream.SendEventAsync(notification, TicketStream.TicketUpdated);
-        }
-    }
-}
+//namespace AutoMais.Ticket.Core.Application.Ticket.Events
+//{
+//    internal class TicketProductsChangedEventHandler(IStream stream) : INotificationHandler<TicketProductsChanged>
+//    {
+//        public Task Handle(TicketProductsChanged notification, CancellationToken cancellationToken)
+//        {
+//            return stream.SendEventAsync(notification, TicketStream.TicketUpdated);
+//        }
+//    }
+//}
